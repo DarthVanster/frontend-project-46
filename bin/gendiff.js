959b151436b3dc2082a4c45acd537dc21fb60a62
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2024-01-21 11:40:21 +0400

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import genDiff from '../src/index.js';
 
-const program = new Command();
+import Commander from 'commander';
+import gendiff from '../index.js';
 
-program
+const { Command } = Commander;
+const gendiffProgram = new Command();
+
+gendiffProgram
+  .version('0.0.1')
   .name('gendiff')
-  .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepat1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format (default: "stylish")')
-  .action((filepath1, filepath2, option) => {
-    const result = genDiff(filepath1, filepath2, option.format);
-    console.log(result);
-  });
-
-program.parse();
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => (
+    console.log(gendiff(filepath1, filepath2, options.format))))
+  .parse(process.argv);
