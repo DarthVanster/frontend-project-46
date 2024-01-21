045112a43ae5117bc889d12db40df002a9547066
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2024-01-21 11:41:46 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import makeFormat from './formatters/index.js';
-import { getDifferentObject, readFile, getExtension } from './utils.js';
+import fs from 'fs';
+import genDiffNodes from './genDiffNodes.js';
 import parse from './parsers.js';
+import formatOutput from './formatters/index.js';
+import { normalizePath, getTypeOfFile } from './utils.js';
 
-function genDiff(filepath1, filepath2, format = 'stylish') {
-  const dataFile1 = readFile(filepath1);
-  const dataFile2 = readFile(filepath2);
-  const extension1 = getExtension(filepath1);
-  const extension2 = getExtension(filepath2);
-  const parseFile1 = parse(dataFile1, extension1);
-  const parseFile2 = parse(dataFile2, extension2);
-  const dataDiff = getDifferentObject(parseFile1, parseFile2);
-  const result = makeFormat(dataDiff, format);
-  return result;
-}
+const gendiff = (dataStr1, dataStr2, format = 'stylish') => {
+  const diffNodes = genDiffNodes(dataStr1, dataStr2);
+  const formattedString = formatOutput(diffNodes, format);
+  return formattedString;
+};
 
-export default genDiff;
+const gendiffFromFiles = (filepath1, filepath2, format) => {
+  const file1Data = fs.readFileSync(normalizePath(filepath1), 'utf-8');
+  const file2Data = fs.readFileSync(normalizePath(filepath2), 'utf-8');
+  const type1 = getTypeOfFile(filepath1);
+  const type2 = getTypeOfFile(filepath2);
+  const parsedData1 = parse(type1, file1Data);
+  const parsedData2 = parse(type2, file2Data);
+  return gendiff(parsedData1, parsedData2, format);
+};
+
+export default gendiffFromFiles;
