d9268eb22ab8844e1be549388dbe2fb05e797ee4
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2024-01-23 21:21:54 +0400

    Delete src directory

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-const normalizePath = (filePath) => (
-  path.isAbsolute(filePath) ? filePath : path.resolve(process.cwd(), filePath)
-);
-
-const getTypeOfFile = (filepath) => path.extname(filepath);
-
-export { normalizePath, getTypeOfFile };
