d55c991bef28a0b72c5c1118cc1bd94758c674c5
DarthVanster <alex14fs@mail.ru>
2024-02-01 20:18:11 +0400

    test11

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,6 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .name('gendiff')
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepat1>')
